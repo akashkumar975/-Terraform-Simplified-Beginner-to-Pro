17a7e35be4b5522198664de0f77c3ce651d7b01c
akash <akashkumar975@outlook.com>
2024-12-13 21:03:40 +0000

    updated day 10

--- Day-10/main.tf
+++ Day-10/main.tf
@@ -6,12 +6,7 @@ terraform {
     }
   }
 
-  backend "azurerm" {
-    resource_group_name  = "StorageAccount-ResourceGroup" # Can be passed via `-backend-config=`"resource_group_name=<resource group name>"` in the `init` command.
-    storage_account_name = "tfstorageaccnt423"            # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
-    container_name       = "tfstate"                      # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
-    key                  = "prod.terraform.tfstate"       # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
-  }
+
 }
 
 provider "azurerm" {
